f614aa94733d5c82903016d01d0198c0bb9bc459
Mikhail Doshevsky <m.doshevsky@gmail.com>
2024-05-16 15:36:11 +0300

    Added instrumentation

--- src/alpha-service/Program.cs
+++ src/alpha-service/Program.cs
@@ -1,6 +1,24 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName))
+        .AddOtlpExporter();
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(x => x
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter());
 
 WebApplication app = builder.Build();
 app.UseSwagger();
